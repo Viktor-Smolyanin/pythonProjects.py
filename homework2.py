b2e310c71149f491301b1beece8754deedfec0b7
Viktor-Smolyanin <viktorsmolyanin@gmail.com>
2024-07-30 20:00:01 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,5 +1,5 @@
 name = 12
 hours = 1.5
 course = 'Python'
-time = 0.125
-print('Курс:',course,',всего задач:',name,',затрачено часов:',hours,',среднее время выполнения',time,'часа.')
+time = (hours/name)
+print('Курс:',course,'всего задач:',name,',затрачено часов:',hours,',среднее время выполнения:',time,'часа.')
