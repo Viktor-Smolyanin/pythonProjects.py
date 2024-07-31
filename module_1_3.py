31978aa1b61d07d536dc4a87b76488bb6e79f0a5
Viktor-Smolyanin <viktorsmolyanin@gmail.com>
2024-07-31 21:58:11 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -1,5 +1,5 @@
 name = 'John'
 age = 25
 new_age = (age+1)
-is_student = 'True'
-print('Name:',name,'age:',age,'new age:',new_age,'Is student:',is_student)
+is_student = (2>1)
+print('Name:',name,'age:',age,'new age:',new_age,'Is student:',is_student,type(is_student))
