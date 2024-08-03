c76c86d32de99da71731dd118170450f985ec8da
Viktor-Smolyanin <viktorsmolyanin@gmail.com>
2024-08-03 16:02:09 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict = {'Vasya': 1975, 'Egor': 1999, 'Masha': 2002}
+print(my_dict)
+print(my_dict['Masha'])
+my_dict['Masha'] = None
+print(my_dict['Masha'])
+my_dict['Masha'] = 2002
+my_dict.update({'Artem': 1915, 'Kamila': 1981})
+del my_dict['Egor']
+print(my_dict)
+my_set = {1, 'Яблоко', 42.314,1, 'Яблоко', 42.314}
+print(my_set)
+my_set.update([13,(5,6,1.6)])
+print(my_set)
+my_set.remove(1)
+print(my_set)
