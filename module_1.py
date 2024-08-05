02143c99ad6494fcd4799acdc0734cd772ebaadf
Viktor-Smolyanin <viktorsmolyanin@gmail.com>
2024-08-05 22:12:54 +0300

    Add files via upload

--- module_1.py
+++ module_1.py
@@ -1,5 +1,5 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-students_sorted = sorted(students)
-average_rate = {students_sorted[0]: sum(grades[0]) / 5, students_sorted[1]: sum(grades[1]) / 4, students_sorted[2]: sum(grades[2]) / 4, students_sorted[3]: sum(grades[3]) / 3, students_sorted[4]: sum(grades[4]) / 5}
+students_1 = sorted(students)
+average_rate = {students_1[0]: sum(grades[0]) / len(grades[0]), students_1[1]: sum(grades[1]) / len(grades[1]), students_1[2]: sum(grades[2]) / len(grades[2]), students_1[3]: sum(grades[3]) / len(grades[3]), students_1[4]: sum(grades[4]) / len(grades[4])}
 print(average_rate)
