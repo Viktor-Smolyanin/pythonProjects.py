268b5bc188e19ed69caf815233412da5cb20d85f
Viktor-Smolyanin <viktorsmolyanin@gmail.com>
2024-08-30 15:32:48 +0300

    Add files via upload

---
+++ module_4.0.py
@@ -0,0 +1,10 @@
+import fake_math as fm
+import true_math as tm
+result5 = fm.fake_divide(33, 3)
+result6 = fm.fake_divide(54, 0)
+result7 = tm.true_divide(45, 5)
+result8 = tm.true_divide(15, 0)
+print(result5)
+print(result6)
+print(result7)
+print(result8)
